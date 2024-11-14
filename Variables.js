2da530a4639a95b731bda30aaccafe6d1eba2695
FatmanaK <janalikbolsin@gmail.com>
2024-11-13 19:32:57 -0600

    add2

--- Variables.js
+++ Variables.js
@@ -8,10 +8,31 @@ console.log("============================================================");
 let value= 100;
 console.log(value);
 console.log(typeof value);
+
 console.log("============================================================");
+
 value=7.5;
 console.log(value);
 console.log(typeof value);
 
 //let vs var:
+
+if(true){
+    // let num = 300;
+    var num = 300;
+ }
+ 
+ console.log(num);
+ 
+ //let country = "Turkey";
+ 
+ //let country = "Japan";
+ 
+ console.log("---------------------------------------");
+ 
+ const MAX_SPEED = 70;
+ 
+ MAX_SPEED = 100;
+ 
+ console.log(MAX_SPEED);
 
