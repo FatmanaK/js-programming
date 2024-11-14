b4ac791ccab4ab678d686fa6c82fbda6009c8562
FatmanaK <janalikbolsin@gmail.com>
2024-11-13 18:58:06 -0600

    variables

---
+++ Variables.js
@@ -0,0 +1,13 @@
+//var variableName=Data;
+var schoolName = "Cydeo";
+console.log(schoolName)
+console.log(typeof schoolName);
+
+console.log("============================================================");
+
+let value= 100;
+console.log(value);
+console.log(typeof value);
+
+//const 
+
